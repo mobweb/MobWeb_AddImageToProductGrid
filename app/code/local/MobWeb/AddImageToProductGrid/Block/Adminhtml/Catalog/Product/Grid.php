1b51d55302e36c204c510022f6da92ca1f2a207f
Louis Bataillard <info@mobweb.ch>
2014-04-12 17:16:52 +0200

    initial commit

---
+++ app/code/local/MobWeb/AddImageToProductGrid/Block/Adminhtml/Catalog/Product/Grid.php
@@ -0,0 +1,37 @@
+<?php
+class MobWeb_AddImageToProductGrid_Block_Adminhtml_Catalog_Product_Grid extends Mage_Adminhtml_Block_Catalog_Product_Grid
+{
+    public function setCollection($collection)
+    {
+        $store = $this->_getStore();
+
+        if ($store->getId() && !isset($this->_joinAttributes['image'])) {
+            $collection->joinAttribute(
+                'image',
+                'catalog_product/image',
+                'entity_id',
+                null,
+                'left',
+                $store->getId()
+            );
+        }
+        else {
+            $collection->addAttributeToSelect('image');
+        }
+
+        parent::setCollection($collection);
+    }
+    
+    protected function _prepareColumns()
+    {
+        $this->addColumn('image', array(
+            'header' => Mage::helper('catalog')->__('Image'),
+            'align' => 'left',
+            'index' => 'image',
+            'width'     => '70',
+            'renderer' => 'MobWeb_AddImageToProductGrid_Block_Adminhtml_Template_Grid_Renderer_Image'
+        ));
+
+        return parent::_prepareColumns();
+    }
+}
---
+++ app/code/local/MobWeb/AddImageToProductGrid/Block/Adminhtml/Template/Grid/Renderer/Image.php
@@ -0,0 +1,13 @@
+<?php 
+class MobWeb_AddImageToProductGrid_Block_Adminhtml_Template_Grid_Renderer_Image extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+    public function render(Varien_Object $row)
+    {
+        $img = $row->getData($this->getColumn()->getIndex());
+        if(strpos($img, 'no_selection') !== 0) {
+            return sprintf('<img src="%s" width="60px" />', Mage::getBaseUrl('media') . 'catalog/product' . $img);
+        }
+
+        return '';
+    }
+}
